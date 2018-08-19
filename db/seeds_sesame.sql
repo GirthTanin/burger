f6ba6e8d0f773c62260fa3b7b304747ba9004028
Jonathan McCarthy <JonathanFMcCarthy@gmail.com>
2018-08-18 23:57:00 -0400

    Here is the framework

---
+++ db/seeds_sesame.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Gooey Lewis", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Childhood Summer", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Gooey Lewis and the News", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Yellow Submarine", false);
